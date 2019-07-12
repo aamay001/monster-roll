568e260d8a6eaa8eaba305efe5ccfbbb2f83f43c
Andy Amaya <amayaa@Andys-MacBook-Pro.local>
2019-07-12 10:51:48 -0700

    Updates

---
+++ precache-manifest.8127311cf646397db9ef966275635e07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bf1be35fd122d5774935f1523e6a2f9",
+    "url": "/monster-roll/index.html"
+  },
+  {
+    "revision": "421cc752b9f06ff81630",
+    "url": "/monster-roll/static/css/main.a57554a8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-roll/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "421cc752b9f06ff81630",
+    "url": "/monster-roll/static/js/main.40a1dd36.chunk.js"
+  },
+  {
+    "revision": "8f0ef239b84f9902a0c5",
+    "url": "/monster-roll/static/js/runtime~main.9fde814b.js"
+  }
+]);
